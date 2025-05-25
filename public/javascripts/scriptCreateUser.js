493f1c262024e09ec4f109172b0bcf1a7d3a5bff
Yvon314 <PointMathematique314@free.fr>
2025-05-25 11:22:11 +0200

    vérifie mot de passe

--- public/javascripts/scriptCreateUser.js
+++ public/javascripts/scriptCreateUser.js
@@ -1,20 +1,31 @@
-function verifForm(){
+function verifForm() {
     //alert(password.value==password_confirm.value)
     console.log(password_confirm.value)
-    const TAILLE_PASSW =1; //debug 12;
-    
+    const TAILLE_PASSW = 7; //debug METTRE 12;
 
-    if (password.value!=password_confirm.value){
+
+    if (password.value != password_confirm.value) {
         alert("erreur:le mot de passe et mot de passe confirmé sont différents")
         return false;
     }
 
-    if ( password.value.length < TAILLE_PASSW ){
-        alert("le mot de passe doit contenir au moins ",TAILLE_PASSW," caractères")
+    if (!(
+        password.value.match(/[0-9]/g) &&
+        password.value.match(/[A-Z]/g) &&
+        password.value.match(/[a-z]/g) &&
+        password.value.match(/[^a-zA-Z\d]/g)
+    )) {
+        alert("le mot de passe doit contenir au moins " + TAILLE_PASSW + " caractères, 1 majuscule, 1 minuscule, 1 caractère spécial, 1 chiiffre");
         return false;
     }
 
-    // alert("debug bloqué");return false;
+
+    if (password.value.length < TAILLE_PASSW) {
+        alert("le mot de passe doit contenir au moins " + TAILLE_PASSW + " caractères")
+        return false;
+    }
+
+    // alert("debug bloqué"); return false;
 
     return true
 }
